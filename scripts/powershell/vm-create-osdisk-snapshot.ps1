655167d3670471ad21fb689de66a075e205aa131
Oumar <oumarsharif@gmail.com>
2022-07-25 01:04:04 +0200

    Add scripts for: - creating a snapshot form an os disk - creating a VM with a specific plan form an os disk

---
+++ scripts/powershell/vm-create-osdisk-snapshot.ps1
@@ -0,0 +1,32 @@
+Param (
+    [Parameter(Mandatory, HelpMessage = "Please provide a valid virtual machine name")]
+    [string] $vmName,
+    [Parameter(Mandatory, HelpMessage = "Please provide a valid snapshot name")]
+    [string] $snapshotName,
+    [Parameter(Mandatory, HelpMessage = "Please provide a valid resource group name")]
+    [string] $resourceGroupName
+)
+
+#Set parameters
+#$vmName = 'odaWindows2019'
+#$snapshotName = 'snapshot-disk-odaWindows2019'
+#$resourceGroupName = 'rg-oda-vm-backup'
+
+#Get the VM object
+Write-Host "Retrieving the virtual machine object ..."
+$vm = Get-AzVM -Name $vmName -ResourceGroupName $resourceGroupName
+
+#Get the OS disk object
+Write-Host "Retrieving the disk object ..."
+$disk = Get-AzDisk -ResourceGroupName $resourceGroupName -DiskName $vm.StorageProfile.OsDisk.Name
+# Create the snapshot in the same location as the disk
+$location = $disk.Location
+
+#Create the snapshot configuration (Add -AccountType Premium_LRS to use the snapshot to create a VM that needs high-performing)
+$snapshotConfig = New-AzSnapshotConfig -SourceUri $disk.Id -OsType Windows -CreateOption Copy -Location $location
+
+#Take the snapshot
+Write-Host "Creating the snapshot ..."
+$snapshot = New-AzSnapshot -Snapshot $snapshotConfig -SnapshotName $snapshotName -ResourceGroupName $resourceGroupName
+
+Write-Host "The snaphot $($snapshot.Name) has been successfully created from disk $($disk.Name)" -ForegroundColor Green
---
+++ scripts/powershell/vm-create-with-plan.ps1
@@ -0,0 +1,35 @@
+Param (
+    [Parameter(Mandatory, HelpMessage = "Please provide a valid virtual machine name")]
+    [string] $vmName,
+    [Parameter(Mandatory, HelpMessage = "Please provide a valid disk name")]
+    [string] $osDiskName,
+    [Parameter(Mandatory, HelpMessage = "Please provide a valid network interface name")]
+    [string] $nicName,
+    [Parameter(Mandatory, HelpMessage = "Please provide a valid resource group name")]
+    [string] $resourceGroupName
+)
+
+$vmName = "SGLOBA-I0005"
+$osDiskName = 'SGLOBA-I0005-OsDisk'
+$nicName = 'vnicSGLOBA-I0005'
+$resourceGroupName = 'RG_GROUPBASTION_PPD'
+
+# Get the OsDisk object
+Write-Host "Retrieving the OS Disk ..."
+$osDisk = Get-AzDisk -ResourceGroupName $resourceGroupName -DiskName $osDiskName
+$location = $osDisk.Location
+
+# Get the existing network interface object
+Write-Host "Retrieving the network interface ..."
+$nic = Get-AzNetworkInterface -Name $nicName -ResourceGroupName $resourceGroupName
+
+# Creating the virtual machine configuration
+$vmConfig = New-AzVMConfig -VMName $vmName -VMSize "Standard_D4s_v3" | Set-AzVMPlan -Publisher "wallix" -Product "wallixbastion" -Name "bastion"
+$vm = Add-AzVMNetworkInterface -VM $vmConfig -Id $nic.Id
+$vm = Set-AzVMOSDisk -VM $vm1 -ManagedDiskId $osDisk.Id -StorageAccountType Standard_LRS -DiskSizeInGB 128 -CreateOption Attach -Linux
+
+# Creating the new virtual machine
+Write-Host "Creating the new virtual machine ..."
+New-AzVM -ResourceGroupName $resourceGroupName -Location $location -VM $vm
+
+Write-Host "The virtual machine has been successfully created" -ForegroundColor Green
