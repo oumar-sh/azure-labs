ac21da43ebb80bdbf51bf173f7aba6a5235fa8ac
Oumar <oumarsharif@gmail.com>
2022-07-31 20:37:55 +0200

    [Mslearn Powershell] Add control on the destination file

--- scripts/powershell/training-mslearn/Backup.ps1
+++ scripts/powershell/training-mslearn/Backup.ps1
@@ -3,11 +3,18 @@ Param(
     [string]$Destination = './.'
 )
 
-If(-Not (Test-Path $Path))
+If (-Not (Test-Path $Path))
 {
     Throw "The source directory $Path does not exist, please specifiy an existing directory"
 }
 
 $date = Get-Date -format "yyyy-MM-dd"
-Compress-Archive -Path $Path -CompressionLevel 'Fastest' -Destination "$($DestinationPath + 'backup-' + $date)"
-Write-Host "Created backup at $($DestinationPath + 'backup-' + $date + '.zip')"
+$DestinationFile = "$($DestinationPath + 'backup-' + $date + '.zip')"
+
+If (-Not (Test-Path $DestinationFile))
+{
+    Compress-Archive -Path $Path -CompressionLevel 'Fastest' -Destination "$($DestinationPath + 'backup-' + $date)"
+    Write-Host "Created backup at $DestinationFile"
+} Else {
+    Write-Error "Today's backup already exists"
+}
