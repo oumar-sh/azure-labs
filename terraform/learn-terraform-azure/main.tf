f9aac6a48c3fe53cc263993149366298b77da151
Oumar <oumarsharif@gmail.com>
2023-03-22 13:23:23 +0100

    #terraform Create a virtual network on Azure Add tags to the resource group

--- terraform/learn-terraform-azure/main.tf
+++ terraform/learn-terraform-azure/main.tf
@@ -14,7 +14,20 @@ provider "azurerm" {
   features {}
 }
 
+# Create a resource group
 resource "azurerm_resource_group" "rg" {
   name     = "terraform-lab-rg"
   location = "francecentral"
+  tags = {
+    Environment = "DEV",
+    Team = "DEVOPS"
+  }
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-lab-terraform"
+  address_space       = ["10.0.0.0/16"]
+  location            = "francecentral"
+  resource_group_name = azurerm_resource_group.rg.name
 }
