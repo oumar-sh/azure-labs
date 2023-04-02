777d5665353dd7c10ead9676551bfd11dc80a6ec
Oumar <oumarsharif@gmail.com>
2023-04-02 19:11:32 +0200

    #terraform hashicups coffe ingredients

--- terraform/learn-terraform-hashicups-provider/main.tf
+++ terraform/learn-terraform-hashicups-provider/main.tf
@@ -30,6 +30,15 @@ resource "hashicups_order" "edu" {
   }
 }
 
+data "hashicups_ingredients" "first_coffee" {
+  coffee_id = hashicups_order.edu.items[0].coffee[0].id
+}
+
+output "first_coffee_ingredients" {
+  value = data.hashicups_ingredients.first_coffee
+}
+
+
 output "edu_order" {
   value = hashicups_order.edu
 }
